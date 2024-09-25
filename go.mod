659bc054ce36727b4572c30da18f18e68ba8b70d
zlatanned <shahiakshay31@gmail.com>
2024-09-25 16:58:10 +0530

    Cleanup activity

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
-module github.com/zlatanned/go-rest-mongo
+module go-rest-mongo
 
-go 1.20
-
-require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
-)
+go 1.22.3
--- main.go
+++ main.go
@@ -1,26 +1,4 @@
 package main
 
-import (
-	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2"
-	"net/http"
-
-	"github.com/zlatanned/go-rest-mongo/controllers"
-)
-
 func main() {
-	r := httprouter.New()
-	uc := controllers.NewUserController(getSession())
-	r.GET("/user/:id", uc.GetUser)
-	r.POST("/user", uc.CreateUser)
-	r.DELETE("/user/:id", uc.DeleteUser)
-	http.ListenAndServe("localhost:8008", r)
-}
-
-func getSession() *mgo.Session{
-	s, err := mgo.Dial("mongodb://localhost:27017")
-	if err != nil{
-		panic(err)
-	}
-	return s
 }
